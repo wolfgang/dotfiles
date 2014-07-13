23051b956b10059288d4dd98de4861dcde181412
wolfgang <wolfgang.deutsch@socialspiel.com>
2014-07-13 19:49:41 +0200

    add org-shared directory

--- .emacs.d_elisp/org-config/setup_directories.el
+++ .emacs.d_elisp/org-config/setup_directories.el
@@ -3,12 +3,14 @@
 (if (eq system-type 'windows-nt)
   (progn
     (setq org-directory "e:/Library/Dropbox/org")
+    (setq org-shared-directory "e:/Library/Dropbox/org-shared")
     (setq org-mobile-directory "e:/Library/Dropbox/OrgMobile")
     (setq org-mobile-inbox-for-pull (org-file "inbox_r.org")))
   (progn
     (setq org-directory "~/Dropbox/org")
+    (setq org-shared-directory "~/Dropbox/org-shared")
     (setq org-mobile-directory "")
   )
 )
 
-(setq org-agenda-files (list org-directory))
+(setq org-agenda-files (list org-directory org-shared-directory))
