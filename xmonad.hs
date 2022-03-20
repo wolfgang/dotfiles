9922ab4ac2e0ba37f0334f9a525a14253980de62
Wolfgang Deutsch <wolfgang.deutsch@bloomdiagnostics.com>
2022-03-20 13:03:35 +0100

    xmonad: Configure layout

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,9 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.StatusBar
+import XMonad.Layout.NoBorders (noBorders, smartBorders)
+import XMonad.Layout.TwoPane
+import XMonad.Layout.Spacing
 
 
 main :: IO ()
@@ -10,11 +13,31 @@ main = xmonad $ myConfig
 
 myConfig = def
     { startupHook = setWMName "LG3D"}
-	{ modMask = mod4Mask 
-	}
-	`additionalKeysP`
-	[ ("M-e" , spawn "emacs")
-	,  ("M-s" , spawn "slack")
-	, ("M-b" , spawn "brave")
-	, ("M-S-l", spawn "xscreensaver-command -lock")
-	] 
+    { modMask = mod4Mask
+    , layoutHook = myLayoutHook 
+    }
+    `additionalKeysP`
+    [ ("M-e" , spawn "emacs")
+    , ("M-s" , spawn "slack")
+    , ("M-b" , spawn "brave")
+    , ("M-S-l", spawn "xscreensaver-command -lock")
+    , ("M-S-d", sendMessage $ JumpToLayout "Tall")
+    , ("M-S-f", sendMessage $ JumpToLayout "Full")
+    , ("M-S-t", sendMessage $ JumpToLayout "TwoPane")
+    ] 
+
+myLayoutHook =
+    smartBorders $ 
+    tiled 
+    ||| Mirror tiled 
+    ||| noBorders Full
+    ||| twoPane
+    ||| Mirror twoPane
+    
+  where
+    twoPane  = TwoPane delta smaster
+    tiled    = Tall nmaster delta ratio
+    smaster  = 50/100
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
