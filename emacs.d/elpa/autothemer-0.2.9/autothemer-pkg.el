1be4720b02581740e859ee435c98d0f7aebbfb1c
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2022-09-18 16:23:46 +0200

    Emacs: Upgrade packages

--- emacs.d/elpa/autothemer-0.2.9/autothemer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from autothemer-0.2.9/autothemer.el  -*- no-byte-compile: t -*-
-(define-package "autothemer" "0.2.9" "Conveniently define themes" '((dash "2.10.0") (emacs "26.1")) :stars nil :commit "378f8707bdd944f19b6fd4544359e993c9113752" :authors '(("Sebastian Sturm")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com") :url "https://github.com/jasonm23/autothemer")
