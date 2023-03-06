c326cd7d1ee73cb78fd7773c4e3464aea228f21a
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2023-03-06 19:35:55 +0100

    Emacs: Fix function declaration order

--- emacs.d/elisp/org/setup_directories.el
+++ emacs.d/elisp/org/setup_directories.el
@@ -7,10 +7,11 @@
     (setq org-mobile-directory "e:/Library/Dropbox/OrgMobile")
     (setq org-mobile-inbox-for-pull (org-file "inbox_r.org"))))
 
+(defun make-org-roam-daily-dir ()
+  (if (eq nil org-roam-directory) nil
+    (concat org-roam-directory "/daily" )))
+
 (setq org-agenda-files (remove-if (lambda (d) (eq nil d )) (list org-directory org-shared-directory org-roam-directory (make-org-roam-daily-dir))))
 (setq org-journal-dir (concat org-directory "/journal"))
 
-(defun make-org-roam-daily-dir ()
-  (if (eq nil org-roam-directory)
-      nil
-    (concat org-roam-directory "/" org-roam-dailies-directory)))
+
