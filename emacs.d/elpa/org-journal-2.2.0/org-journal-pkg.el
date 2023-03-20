ec5ea0945d89d5727a77b3e2faa0829321bed125
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2023-03-20 19:32:22 +0100

    Emacs: Update packages

---
+++ emacs.d/elpa/org-journal-2.2.0/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-journal.el  -*- no-byte-compile: t -*-
+(define-package "org-journal" "2.2.0" "a simple org-mode based journaling mode" '((emacs "25.1") (org "9.1")) :stars nil :commit "18df4d5ae5e15580df42562c143d007c6d28d75f" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
