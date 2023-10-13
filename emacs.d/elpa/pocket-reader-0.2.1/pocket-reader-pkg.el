2f73fff259d8d79d29f307b05ef9a9fa2259b095
Wolfgang <wolfgang.deutsch@gmail.com>
2023-10-13 11:51:30 +0200

    Emacs: Upgrade packages

--- emacs.d/elpa/pocket-reader-0.2.1/pocket-reader-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from pocket-reader.el  -*- no-byte-compile: t -*-
-(define-package "pocket-reader" "0.2.1" "Client for Pocket reading list" '((emacs "25.1") (dash "2.13.0") (kv "0.0.19") (pocket-lib "0.1") (s "1.10") (ov "1.0.6") (rainbow-identifiers "0.2.2") (org-web-tools "0.1") (ht "2.2")) :commit "dbb7b043edb42341b7f48ce0d81ba08c3c676076" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("pocket") :url "https://github.com/alphapapa/pocket-reader.el")
