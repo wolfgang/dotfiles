7b7b07cf84b94a56ede9e3642efd3a98b9c6c904
Wolfgang <wolfgang.deutsch@gmail.com>
2023-11-04 21:39:52 +0100

    Emacs: Upgrade packages

---
+++ emacs.d/elpa/tree-sitter-langs-0.12.75/tree-sitter-langs-pkg.el
@@ -0,0 +1,15 @@
+(define-package "tree-sitter-langs" "0.12.75" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "3aa69b53594038c0d41b70144e74cf4619c7a48c" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
