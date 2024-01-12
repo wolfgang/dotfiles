02ae8ff0e379e471e55d2bc4ece186c1f761a3bc
Wolfgang <wolfgang.deutsch@gmail.com>
2024-01-12 14:59:43 +0100

    Emacs: Update packages

---
+++ emacs.d/elpa/consult-1.1/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "1.1" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "0e7f3ec8e7e959e262158fa4ddb73de80b52ccc0" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
