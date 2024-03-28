39724a9ccd13224aee93269d46f21a6e64e09701
Wolfgang Deutsch <wolfgang.deutsch@smatrics.com>
2024-03-28 08:27:06 +0100

    Emacs: Upgrade packages

---
+++ emacs.d/elpa/ledger-mode-20240326.1834/ledger-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ledger-mode" "20240326.1834" "Helper code for use with the \"ledger\" command-line tool"
+  '((emacs "25.1"))
+  :commit "e6946feced023c223650ac31cbe5c96bea2a7afb")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
