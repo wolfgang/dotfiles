7ccbdc1d47d4e7cb3f67e9e5a3e8ba951a354d6c
Wolfgang Deutsch <wolfgang.deutsch@smatrics.com>
2024-06-13 09:35:51 +0200

    Emacs: Update packages

---
+++ emacs/.emacs.d/elpa/plz-0.9/plz-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from plz.el  -*- no-byte-compile: t -*-
+(define-package "plz" "0.9" "HTTP library" '((emacs "27.1")) :commit "399ad3e1aafa7b86c1ce0cfab8bcf2e09cb0e956" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("comm" "network" "http") :url "https://github.com/alphapapa/plz.el")
---
+++ emacs/.emacs.d/elpa/racket-mode-20240607.1211/racket-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "racket-mode" "20240607.1211" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "ac5ede46b55b7602548c895272c8ab9c82053b2f" :authors
+  '(("Greg Hendershott" . "racket-mode-author@greghendershott.com"))
+  :maintainers
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
