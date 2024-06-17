0c301aa7b4f5ff40cbf5b0d0e85da53721aea573
Wolfgang <wolfgang.deutsch@gmail.com>
2024-06-17 17:08:01 +0200

    Emacs: Upgrade packages

---
+++ emacs/.emacs.d/elpa/racket-mode-20240612.1256/racket-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "racket-mode" "20240612.1256" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "4cdbc1deb30c1625fadde762e55eb78c3acede53" :authors
+  '(("Greg Hendershott" . "racket-mode-author@greghendershott.com"))
+  :maintainers
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/.emacs.d/elpa/tree-sitter-langs-0.12.201/tree-sitter-langs-pkg.el
@@ -0,0 +1,15 @@
+(define-package "tree-sitter-langs" "0.12.201" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "a06804e13fc1d24d0099b44ce23d6ac5e6bac5b8" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
