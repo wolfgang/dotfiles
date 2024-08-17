4f6581b003f157b07cc7adfc4ba63ef5124c3a15
Wolfgang <wolfgang.deutsch@gmail.com>
2024-08-17 18:26:37 +0200

    Emacs: Fix org directory setup

--- emacs/.emacs.d/elisp/org/setup_directories.el
+++ emacs/.emacs.d/elisp/org/setup_directories.el
@@ -7,11 +7,8 @@
     (setq org-mobile-directory "e:/Library/Dropbox/OrgMobile")
     (setq org-mobile-inbox-for-pull (org-file "inbox_r.org"))))
 
-(defun make-org-roam-daily-dir ()
-  (if (eq nil org-roam-directory) nil
-    (concat org-roam-directory "/daily" )))
-
-(setq org-agenda-files (remove-if (lambda (d) (eq nil d )) (list org-directory org-shared-directory org-roam-directory denote-directory (make-org-roam-daily-dir))))
+(setq org-agenda-files (list org-directory denote-directory))
+(when (boundp 'org-shared-directory) (push org-shared-directory org-agenda-files))
 (setq org-journal-dir (concat org-directory "/journal"))
 
 
