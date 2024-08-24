5344f02776f0875113ed62cd6aa217150ba1667f
Wolfgang <wolfgang.deutsch@gmail.com>
2024-08-24 16:36:07 +0200

    Emacs: Upgrade packages

--- emacs/.emacs.d/elpa/plz-0.8/plz-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from plz.el  -*- no-byte-compile: t -*-
-(define-package "plz" "0.8" "HTTP library" '((emacs "26.3")) :commit "57f10482655f02c3cc8ff35d31bd83dd6d809d02" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("comm" "network" "http") :url "https://github.com/alphapapa/plz.el")
--- emacs/.emacs.d/elpa/racket-mode-20240810.1326/racket-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "racket-mode" "20240810.1326" "Racket editing, REPL, and more"
-  '((emacs "25.1"))
-  :commit "7190f9c8634a5b06554d518b1835460a432dabbf" :authors
-  '(("Greg Hendershott" . "racket-mode-author@greghendershott.com"))
-  :url "https://www.racket-mode.com/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
