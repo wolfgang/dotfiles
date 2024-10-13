163a2ec5c56408f08bbe28e667c0f8a2a4e64ab9
Wolfgang <wolfgang.deutsch@gmail.com>
2024-10-13 13:50:45 +0200

    Emacs: Upgrade packages

---
+++ emacs/.emacs.d/elpa/casual-symbol-overlay-1.1.0/casual-symbol-overlay-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "casual-symbol-overlay" "1.1.0"
+  "Transient UI for Symbol Overlay."
+  '((emacs          "29.1")
+    (casual-lib     "1.1.0")
+    (symbol-overlay "4.2"))
+  :url "https://github.com/kickingvegas/casual-symbol-overlay"
+  :commit "9481daf1b9b027dbd12cfed8219b89e73ec3728a"
+  :revdesc "9481daf1b9b0"
+  :keywords '("tools")
+  :authors '(("Charles Choi" . "kickingvegas@gmail.com"))
+  :maintainers '(("Charles Choi" . "kickingvegas@gmail.com")))
