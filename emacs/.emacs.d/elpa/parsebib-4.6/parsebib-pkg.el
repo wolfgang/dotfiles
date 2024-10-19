d1b7bacac9ccabb60f1b952d55e6b9b2146ad19e
Wolfgang <wolfgang.deutsch@gmail.com>
2024-10-19 19:22:55 +0200

    Emacs: Upgrade packages

---
+++ emacs/.emacs.d/elpa/parsebib-4.6/parsebib-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "parsebib" "4.6"
+  "A library for parsing bib files."
+  '((emacs "25.1"))
+  :url "https://github.com/joostkremers/parsebib"
+  :commit "73fe6204a3f204b45316fc25f17ad035a00acc16"
+  :revdesc "73fe6204a3f2"
+  :keywords '("text" "bibtex")
+  :authors '(("Joost Kremers" . "joostkremers@fastmail.fm"))
+  :maintainers '(("Joost Kremers" . "joostkremers@fastmail.fm")))
