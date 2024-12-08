efd1dad3abd5ee55a81c2bbb1d6ab174c5b143de
Wolfgang Deutsch <wolfgang@wolfgangdeutsch.work>
2024-12-08 12:22:48 +0100

    Emacs: Upgrade packages

--- emacs/.emacs.d/elpa/racket-mode-20241129.1353/racket-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "racket-mode" "20241129.1353"
-  "Racket editing, REPL, and more."
-  '((emacs "25.1"))
-  :url "https://github.com/greghendershott/racket-mode"
-  :commit "986c9ad883a1b96076a0da6ae75761c75baaac09"
-  :revdesc "986c9ad883a1"
-  :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")))
---
+++ emacs/.emacs.d/elpa/racket-mode-20241206.2135/racket-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "racket-mode" "20241206.2135"
+  "Racket editing, REPL, and more."
+  '((emacs "25.1"))
+  :url "https://github.com/greghendershott/racket-mode"
+  :commit "7586b5857ff01bd9f350521112d8efad03e0bb6e"
+  :revdesc "7586b5857ff0"
+  :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")))
