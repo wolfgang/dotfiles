66093fc89f14ccb20e73e4808968615fd9fdd03d
Wolfgang Deutsch <wolfgang@wolfgangdeutsch.work>
2024-12-08 18:44:30 +0100

    Emacs: Add capture template

--- emacs/.emacs.d/elisp/org/capture_templates.el
+++ emacs/.emacs.d/elisp/org/capture_templates.el
@@ -2,8 +2,8 @@
 
 (setq org-capture-templates
       `(("t" "Todo" entry (file+headline ,(org-file "inbox.org") "Inbox") "* TODO %?\n")
-	    ("l" "Log" entry (file+datetree ,(denote-file "20240824T163844--the-log__log.org")) "* %U %?  %i\n")
-	    ("d" "Todo Today" entry (file+headline ,(org-file  "inbox.org") "Inbox") "* SCHD  %?\nSCHEDULED: %t")
+	    ("l" "Personal Log" entry (file+datetree ,(denote-file "20240824T163844--personal-log__journal.org")) "* %U %?  %i\n")
+        ("d" "Dev Log" entry (file+datetree ,(denote-file "20241208T184247--dev-log__dev.org")) "* %U %?  %i\n")
 	    ("c" "[ ] under clocked" checkitem (clock) "[ ]  %?\n")
 	    ("o" "TODO under Clocked" entry (clock) "* TODO  %?\n")
 	    ("s" "Todo (Shared)" entry (file+headline ,(org-shared-file "inbox_shared.org") "Inbox") "* TODO %?\n")))
