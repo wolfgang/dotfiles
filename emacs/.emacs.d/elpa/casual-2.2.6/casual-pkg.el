a4be12cef20831504a8865d3cb0d1b10142ccbc3
Wolfgang Deutsch <wolfgang@wolfgangdeutsch.work>
2024-12-20 19:02:34 +0100

    Emacs: Upgrade packages

---
+++ emacs/.emacs.d/elpa/casual-2.2.6/casual-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "casual" "2.2.6"
+  "Transient user interfaces for various modes."
+  '((emacs     "29.1")
+    (transient "0.6.0"))
+  :url "https://github.com/kickingvegas/casual"
+  :commit "6e63bde2038682422171ad350f58bf56c85e1d31"
+  :revdesc "6e63bde20386"
+  :keywords '("tools" "wp")
+  :authors '(("Charles Choi" . "kickingvegas@gmail.com"))
+  :maintainers '(("Charles Choi" . "kickingvegas@gmail.com")))
