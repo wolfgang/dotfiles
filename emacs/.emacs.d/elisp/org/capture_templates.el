65f15c4035cfcba63cf13eef354eb2ec0146a887
Wolfgang Deutsch <wolfgang@wolfgangdeutsch.work>
2024-12-30 07:40:32 +0100

    Replace deprecated keyword

--- emacs/.emacs.d/elisp/org/capture_templates.el
+++ emacs/.emacs.d/elisp/org/capture_templates.el
@@ -2,8 +2,8 @@
 
 (setq org-capture-templates
       `(("t" "Todo" entry (file+headline ,(org-file "inbox.org") "Inbox") "* TODO %?\n")
-	    ("l" "Personal Log" entry (file+datetree ,(denote-file "20240824T163844--personal-log__journal.org")) "* %U %?  %i\n")
-        ("d" "Dev Log" entry (file+datetree ,(denote-file "20241208T184247--dev-log__dev.org")) "* %U %?  %i\n")
+	    ("l" "Personal Log" entry (file+olp+datetree ,(denote-file "20240824T163844--personal-log__journal.org")) "* %U %?  %i\n")
+        ("d" "Dev Log" entry (file+olp+datetree ,(denote-file "20241208T184247--dev-log__dev.org")) "* %U %?  %i\n")
 	    ("c" "[ ] under clocked" checkitem (clock) "[ ]  %?\n")
 	    ("o" "TODO under Clocked" entry (clock) "* TODO  %?\n")
 	    ("s" "Todo (Shared)" entry (file+headline ,(org-shared-file "inbox_shared.org") "Inbox") "* TODO %?\n")))
