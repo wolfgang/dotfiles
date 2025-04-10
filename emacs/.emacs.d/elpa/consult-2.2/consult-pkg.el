71290b9ef9fab7cba1e2256750d5416f3333cabd
Wolfgang Deutsch <wolfgang@wolfgangdeutsch.work>
2025-04-10 17:57:25 +0200

    emacs: Upgrade packages

---
+++ emacs/.emacs.d/elpa/consult-2.2/consult-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "consult" "2.2"
+  "Consulting completing-read."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/consult"
+  :commit "30a42ac8f3d42f653eda234ee538c1960704f4f2"
+  :revdesc "30a42ac8f3d4"
+  :keywords '("matching" "files" "completion")
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
